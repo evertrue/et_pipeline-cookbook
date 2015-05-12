7e746f58922c2ca3663f0c875326fbde486e7e0b
Eric Herot <eric.github@herot.com>
2015-05-12 16:12:53 -0400

    Attributes file is not needed: We get that data from the data bags

--- attributes/default.rb
+++
@@ -1,4 +0,0 @@
-# set['pipeline']['chef_server']['url'] = "http://0.0.0.0:80"
-# set['pipeline']['chef_server']['node_name'] = "pipeline"
-default['pipeline']['chef-repo']['url'] =
-  'git@github.com:evertrue/server-chef.git'
