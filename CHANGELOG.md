131c1b532ce7f59cc92f20fa3cd5f245d58232e7
Eric Herot <eric.github@herot.com>
2015-05-13 14:31:54 -0400

    Version bump 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
 et_pipeline CHANGELOG
 ======================
 
+v1.0.2 (2015-05-13)
+-------------------
+* Populate the .chef/client.pem key with data from an encrypted data bag
+
 v1.0.1 (2015-05-12)
 -------------------
 * First real release!
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'devops@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures et_pipeline'
 long_description 'Installs/Configures et_pipeline'
-version          '1.0.1'
+version          '1.0.2'
 
 depends 'pipeline'
 depends 'ssh_known_hosts'
